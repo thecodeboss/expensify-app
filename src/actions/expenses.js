5c97c5e8a7474053ad7266cb088909608a88cdf3
Michael Oliver <mikemboliver@gmail.com>
2018-02-11 01:57:18 -0800

    Store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -8,7 +8,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     const {
       description = '',
       note = '',
@@ -17,7 +18,7 @@ export const startAddExpense = (expenseData = {}) => {
     } = expenseData;
     const expense = { description, note, amount, createdAt };
 
-    return database.ref('expenses').push(expense).then((ref) => {
+    return database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
@@ -32,8 +33,9 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
-export const startRemoveExpense = ({ id }) => (dispatch) => {
-  return database.ref(`expenses/${id}`)
+export const startRemoveExpense = ({ id }) => (dispatch, getState) => {
+  const uid = getState().auth.uid;
+  return database.ref(`users/${uid}/expenses/${id}`)
     .remove()
     .then(() => {
       dispatch(removeExpense({ id }));
@@ -47,8 +49,9 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
-export const startEditExpense = (id, updates) => (dispatch) => {
-  return database.ref(`expenses/${id}`)
+export const startEditExpense = (id, updates) => (dispatch, getState) => {
+  const uid = getState().auth.uid;
+  return database.ref(`users/${uid}/expenses/${id}`)
     .update(updates)
     .then(() => {
       dispatch(editExpense(id, updates));
@@ -61,8 +64,9 @@ export const setExpenses = (expenses) => ({
   expenses
 });
 
-export const startSetExpenses = () => (dispatch) => {
-  return database.ref('expenses')
+export const startSetExpenses = () => (dispatch, getState) => {
+  const uid = getState().auth.uid;
+  return database.ref(`users/${uid}/expenses`)
     .once('value')
     .then((snapshot) => {
       const expenses = [];
